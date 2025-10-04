0bd5e2eb34a6c44185337e2c30643aae12e496d7
AlAl <alalwork@protonmail.com>
2025-10-04 19:50:20 -0400

    Scrape job postings location and description on workday

--- src/stackinfo-scrape/scrape.py
+++ src/stackinfo-scrape/scrape.py
@@ -4,18 +4,16 @@ from selenium.webdriver import Firefox
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import StaleElementReferenceException
-import os
-import re
-from datetime import datetime
-import time
+from os import path
+from re import search
 
-home_path = os.path.expanduser("~")
-tor_directory = os.path.join(
+home_path = path.expanduser("~")
+tor_directory = path.join(
     home_path, "src", "stackinfo", "src", "stackinfo-scrape", "tor-browser")
-profile_path = os.path.join(
+profile_path = path.join(
     tor_directory, "Browser", "TorBrowser", "Data", "Browser", "profile.default")
-binary_path = os.path.join(tor_directory, "Browser", "firefox")
-gecko_driver_path = os.path.join(tor_directory, "geckodriver")
+binary_path = path.join(tor_directory, "Browser", "firefox")
+gecko_driver_path = path.join(tor_directory, "geckodriver")
 
 firefox_driver_service = FirefoxService(gecko_driver_path)
 firefox_driver_service.port = 2828
@@ -36,7 +34,7 @@ def wait_for_tor_connection(driver: Firefox):
     wait.until(lambda _: driver.title == "")
 
 
-def attr_value_exists(driver: Firefox, tag: str, atr: str, attr_value: str):
+def attr_value_exists(driver: Firefox, tag: str, atr: str, attr_value: str) -> bool:
     try:
         elements = driver.find_elements(By.TAG_NAME, tag)
         for element in elements:
@@ -53,6 +51,15 @@ def wait_for_attr_value(driver: Firefox, tag: str, atr: str, attr_value: str):
     wait.until(lambda _: attr_value_exists(driver, tag, atr, attr_value))
 
 
+def get_element_by_attr_value(driver: Firefox, tag: str, atr: str, attr_value: str):
+    elements = driver.find_elements(By.TAG_NAME, tag)
+    for element in elements:
+        attribute = element.get_attribute(atr)
+        if attribute == attr_value:
+            return element
+    return None
+
+
 with tor_browser:
     wait_for_tor_connection(tor_browser)
     tor_browser.get("https://att.wd1.myworkdayjobs.com/en-US/ATTCollege")
@@ -61,10 +68,18 @@ with tor_browser:
     hrefs = []
     for element in elements:
         href = element.get_attribute("href")
-        if re.search("/job/", href) is not None:
+        if search("/job/", href) is not None:
             hrefs.append(href)
+    locations = []
+    descriptions = []
     for href in hrefs:
         tor_browser.get(href)
         wait_for_attr_value(tor_browser, "div",
                             "data-automation-id", "job-posting-details")
-
+        location_parent = get_element_by_attr_value(
+            tor_browser, "div", "data-automation-id", "locations")
+        location = location_parent.find_element(By.TAG_NAME, "dd")
+        locations.append(location)
+        description = get_element_by_attr_value(
+            tor_browser, "div", "data-automation-id", "jobPostingDescription")
+        print(description.text)
