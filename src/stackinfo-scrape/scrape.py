774144e5f6c0a6b9ecaf0a3f9f56576779c7fca3
AlAl <alalwork@protonmail.com>
2025-10-03 20:35:46 -0400

    Visit each job posting on the first page of workday

--- src/stackinfo-scrape/scrape.py
+++ src/stackinfo-scrape/scrape.py
@@ -1,42 +1,70 @@
 from selenium.webdriver import FirefoxOptions
 from selenium.webdriver import FirefoxService
-# from undetected_geckodriver import Firefox
 from selenium.webdriver import Firefox
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import StaleElementReferenceException
-
-
 import os
+import re
+from datetime import datetime
+import time
+
+home_path = os.path.expanduser("~")
+tor_directory = os.path.join(
+    home_path, "src", "stackinfo", "src", "stackinfo-scrape", "tor-browser")
+profile_path = os.path.join(
+    tor_directory, "Browser", "TorBrowser", "Data", "Browser", "profile.default")
+binary_path = os.path.join(tor_directory, "Browser", "firefox")
+gecko_driver_path = os.path.join(tor_directory, "geckodriver")
+
+firefox_driver_service = FirefoxService(gecko_driver_path)
+firefox_driver_service.port = 2828
+
+firefox_options = FirefoxOptions()
+firefox_options.log.level = "trace"
+firefox_options.set_preference("marionette.debugging.clicktostart", False)
+firefox_options.set_preference("torbrowser.settings.quickstart.enabled", True)
+firefox_options.binary_location = binary_path
+firefox_options.profile = profile_path
+# firefox_options.add_argument("--headless")
+
+tor_browser = Firefox(firefox_options, firefox_driver_service)
 
 
-firefox = Firefox()
+def wait_for_tor_connection(driver: Firefox):
+    wait = WebDriverWait(driver, 200.0)
+    wait.until(lambda _: driver.title == "")
 
 
-# def wait_for_tor_connection(driver: Firefox):
-#     wait = WebDriverWait(driver, 180)
-#     wait.until(lambda _: driver.title == "")
-
-
-def find_content(driver: Firefox):
+def attr_value_exists(driver: Firefox, tag: str, atr: str, attr_value: str):
     try:
-        elements = driver.find_elements(By.CLASS_NAME, "site_content")
-        if len(elements) != 0:
-            return True
-        else:
-            return False
+        elements = driver.find_elements(By.TAG_NAME, tag)
+        for element in elements:
+            attribute = element.get_attribute(atr)
+            if attribute == attr_value:
+                return True
+        return False
     except StaleElementReferenceException:
         return False
 
 
-def wait_for_site_content(driver: Firefox):
-    wait = WebDriverWait(driver, 180)
-    wait.until(find_content)
-
-# with firefox:
-#     firefox.get(
-#         "hiring.cafe")
-#     wait_for_site_content(firefox)
+def wait_for_attr_value(driver: Firefox, tag: str, atr: str, attr_value: str):
+    wait = WebDriverWait(driver, 180.0)
+    wait.until(lambda _: attr_value_exists(driver, tag, atr, attr_value))
 
 
-firefox.get("hiring.cafe")
+with tor_browser:
+    wait_for_tor_connection(tor_browser)
+    tor_browser.get("https://att.wd1.myworkdayjobs.com/en-US/ATTCollege")
+    wait_for_attr_value(tor_browser, "a", "data-automation-id", "jobTitle")
+    elements = tor_browser.find_elements(By.TAG_NAME, "a")
+    hrefs = []
+    for element in elements:
+        href = element.get_attribute("href")
+        if re.search("/job/", href) is not None:
+            hrefs.append(href)
+    for href in hrefs:
+        tor_browser.get(href)
+        wait_for_attr_value(tor_browser, "div",
+                            "data-automation-id", "job-posting-details")
+
